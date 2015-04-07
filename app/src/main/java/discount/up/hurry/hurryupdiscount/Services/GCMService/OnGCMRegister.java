e9760380292e4aacaf5d0e57e421a4c3bc162475
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-04-06 23:25:56 -0500

    GCM Registration and Update

---
+++ app/src/main/java/discount/up/hurry/hurryupdiscount/Services/GCMService/OnGCMRegister.java
@@ -0,0 +1,8 @@
+package discount.up.hurry.hurryupdiscount.Services.GCMService;
+
+import discount.up.hurry.hurryupdiscount.Models.Mobile;
+
+public interface OnGCMRegister {
+
+    void onGCMRegister(Mobile mobile);
+}
